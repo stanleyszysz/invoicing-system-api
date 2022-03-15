76a96ab58ca06930f2b90f5adcaee9ee6e5c2ce5
stanleyszysz <carracass@gmail.com>
2022-03-15 14:49:08 +0100

    Initial commit

---
+++ src/main/java/pl/fc/invoicing/InvoicingSystemApiApplication.java
@@ -0,0 +1,13 @@
+package pl.fc.invoicing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicingSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicingSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/fc/invoicing/InvoicingSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fc.invoicing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicingSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
